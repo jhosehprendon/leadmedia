3b8a6ec9c5b5ed0c73d95932ea74bd1d97d6c7f4
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-07-03 09:42:12 -0500

    Add email sequence integration with ActiveCampaign

--- src/routers/agencyClient.js
+++ src/routers/agencyClient.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const AgencyClient = require('../models/agencyClient');
+const ActiveCampaign = require("activecampaign");
 
 const router = new express.Router();
 
-//////// AGENCY ROUTES ////////
+//////// AGENCY CLIENT ROUTES ////////
 
 router.post('/agency-client', async (req, res) => {
     const agencyClient = new AgencyClient(req.body)
     try {
         await agencyClient.save()
-        // sendWelcomeEmail(user.email, user.name)
+
         res.status(201).send(agencyClient)
     } catch(e) {
         res.status(400).send(e)
--- src/routers/agencyLead.js
+++ src/routers/agencyLead.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const ActiveCampaign = require("activecampaign");
 const AgencyLead = require('../models/agencyLead');
 
 const router = new express.Router();
+const ac = new ActiveCampaign("https://payjhosehp.api-us1.com", "aa9ace4505ed827783707a3c542f46abd5aee91825de6725112370260201e6708a2abff8");
 
 //////// AGENCY LEAD ROUTES ////////
 
@@ -9,7 +11,22 @@ router.post('/agency-lead', async (req, res) => {
     const agencyLead = new AgencyLead(req.body)
     try {
         await agencyLead.save()
-        // sendWelcomeEmail(user.email, user.name)
+
+        // Active Campaign
+        var contact = {
+            'email': agencyLead.email,
+            'first_name': agencyLead.name,
+            'p[2]': 1,
+            'status[2]': 1,
+        };
+        var contact_add = ac.api("contact/add", contact);
+
+        contact_add.then(function(result) {
+            console.log(result);
+        }, function(result) {
+            console.log(result);
+        });
+
         res.status(201).send(agencyLead)
     } catch(e) {
         res.status(400).send(e)
